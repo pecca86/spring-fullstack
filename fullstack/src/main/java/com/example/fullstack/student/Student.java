81d6f6b1fbfedf5496ee81e9c79113b19265bb60
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-06-14 15:22:55 +0300

    Persisting data

--- fullstack/src/main/java/com/example/fullstack/student/Student.java
+++ fullstack/src/main/java/com/example/fullstack/student/Student.java
@@ -3,18 +3,43 @@ package com.example.fullstack.student;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
     private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    public Student(String firstName, String lastName, String email, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
 }
