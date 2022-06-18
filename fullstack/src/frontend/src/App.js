671609a288625e0add4b0fdbaa7ef1337f94feb2
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-06-18 17:51:43 +0300

    Created a AWS database

--- fullstack/src/frontend/src/App.js
+++ fullstack/src/frontend/src/App.js
@@ -165,19 +165,14 @@ function App() {
             <div className="logo"/>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                 <Menu.Item key="1" icon={<PieChartOutlined/>}>
-                    Option 1
+                    Students
                 </Menu.Item>
                 <Menu.Item key="2" icon={<DesktopOutlined/>}>
-                    Option 2
+                    Teachers
                 </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Timo</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
+                <SubMenu key="sub1" icon={<UserOutlined/>} title="Current User">
+                    <Menu.Item key="3">Log out</Menu.Item>
+                    <Menu.Item key="4">Settings</Menu.Item>
                 </SubMenu>
                 <Menu.Item key="9" icon={<FileOutlined/>}>
                     Files
