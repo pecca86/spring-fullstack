1de02c495b647ec6b79891a6e12588db8404c0f1
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-06-19 15:27:50 +0300

    fis

--- fullstack/src/test/java/com/example/fullstack/FullstackApplicationTests.java
+++ fullstack/src/test/java/com/example/fullstack/FullstackApplicationTests.java
@@ -10,6 +10,8 @@ class FullstackApplicationTests {
     @Test
     void contextLoads() {
 
+        System.out.println("fis ner dej!");
+        System.out.println("ff");
         System.out.println("Some text to make changes...");
         // Make a failing test to check dev pipeline
         Assertions.fail("Test failed");
