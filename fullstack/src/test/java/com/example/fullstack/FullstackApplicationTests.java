0a2f7351781fe20f454fc9b7832bb61cd78b1276
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-06-19 16:40:58 +0300

    cddds

--- fullstack/src/test/java/com/example/fullstack/FullstackApplicationTests.java
+++ fullstack/src/test/java/com/example/fullstack/FullstackApplicationTests.java
@@ -9,7 +9,7 @@ class FullstackApplicationTests {
 
     @Test
     void contextLoads() {
-
+        System.out.println("CDSSSS");
         System.out.println("Some text to make changes...");
         // Make a failing test to check dev pipeline
         Assertions.fail("Test failed");
