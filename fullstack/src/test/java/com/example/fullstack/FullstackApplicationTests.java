cf575c5d623f8fb351816fd06b23ca4332c17925
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-06-20 22:55:32 +0300

    passing test

--- fullstack/src/test/java/com/example/fullstack/FullstackApplicationTests.java
+++ fullstack/src/test/java/com/example/fullstack/FullstackApplicationTests.java
@@ -11,9 +11,6 @@ class FullstackApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("pitten ska ha sitt!");
-        System.out.println("Some text to make changes...");
-        // Make a failing test to check dev pipeline
-        Assertions.fail("FAILING TEST ON PURPOSE");
+        //
     }
 }
