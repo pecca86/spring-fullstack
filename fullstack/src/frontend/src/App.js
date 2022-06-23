115730543fac61977d49dc5b0e1da895ffa8a0d6
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-06-23 11:11:46 +0300

    he

--- fullstack/src/frontend/src/App.js
+++ fullstack/src/frontend/src/App.js
@@ -193,7 +193,7 @@ function App() {
             <Footer style={{textAlign: 'center'}}>
                 By Pekka Solutions ⭐
                 <Divider>
-                    <a href="#">Link schmink</a>
+                    <a href="#">Link schmink pink?</a>
                 </Divider>
                 ️</Footer>
         </Layout>
